88fe9522d44c6a0f475f7594785ba7b662226535
Dirk Steele <dirk.avis@gmail.com>
2025-08-17 09:31:21 -0400

    Restore page files from 2025-08-16 backup

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,3 +1,1 @@
-# This file makes the 'core' directory a Python package.
-# It can be left empty for this project.
-
+﻿
--- pages/drivability_page.py
+++ pages/drivability_page.py
@@ -1,7 +1,5 @@
-# FILE: pages/drivability_page.py
-# PASTE THIS FULL FILE
-
 from __future__ import annotations
+import time
 from typing import Tuple
 from .base_page import BasePage
 
@@ -9,42 +7,24 @@ try:
 	from selenium.webdriver.common.by import By
 except Exception:
 	class _By:
-		CSS_SELECTOR = "css selector"
-		XPATH = "xpath"
-		ID = "id"
-		NAME = "name"
+		CSS_SELECTOR = "css selector"; XPATH = "xpath"; ID = "id"; NAME = "name"
 	By = _By()  # type: ignore
 
-
 class DrivabilityPage(BasePage):
-	"""Represents the Drivability question page (Is the vehicle drivable?)."""
-
 	class S:
-		DIALOG: Tuple[str, str] = (
-			By.CSS_SELECTOR,
-			"div.bp6-dialog[data-testid='drivability-page'], div.fleet-operations-pwa__drivabilityPage",
-		)
-		YES_BTN: Tuple[str, str] = (
-			By.CSS_SELECTOR,
-			"button[data-testid='drivable-yes'], button.fleet-operations-pwa__drivableYes",
-		)
-		NO_BTN: Tuple[str, str] = (
-			By.CSS_SELECTOR,
-			"button[data-testid='drivable-no'], button.fleet-operations-pwa__drivableNo",
-		)
-		NEXT_BTN: Tuple[str, str] = (
-			By.CSS_SELECTOR,
-			"button[data-testid='drivability-next'], button.bp6-button.bp6-intent-primary",
-		)
+		DIALOG: Tuple[str,str] = (By.CSS_SELECTOR, "div.bp6-dialog[data-testid='drivability-page'], div.fleet-operations-pwa__drivabilityPage")
+		YES_BTN: Tuple[str,str] = (By.CSS_SELECTOR, "button[data-testid='drivable-yes'], button.fleet-operations-pwa__drivableYes")
+		NO_BTN:  Tuple[str,str] = (By.CSS_SELECTOR, "button[data-testid='drivable-no'],  button.fleet-operations-pwa__drivableNo")
+		NEXT_BTN:Tuple[str,str] = (By.CSS_SELECTOR, "button[data-testid='drivability-next'], button.bp6-button.bp6-intent-primary")
 
 	def ensure_open(self) -> None:
-		"""Later: wait until page/dialog is visible."""
-		pass
+		self.find(*self.S.DIALOG)
 
 	def select_drivable(self, drivable: bool) -> None:
-		"""Stub: click YES or NO depending on drivable."""
-		pass
+		btn = self.S.YES_BTN if drivable else self.S.NO_BTN
+		self.find(*btn).click()
+		time.sleep(0.3)
 
 	def click_next(self) -> None:
-		"""Stub: continue to the next page (Complaint Type)."""
-		pass
+		self.find(*self.S.NEXT_BTN).click()
+		time.sleep(0.5)
